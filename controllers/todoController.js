ac267ba7ec4f69b7c143a166d9cb83e82f54313c
Piyush1411 <piyushggupta1411@gmail.com>
2023-06-03 17:03:28 +0530

    third commit

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -29,7 +29,7 @@ exports.updateTask = asyncHandler(async (req, res) => {
     const existTask = await Todo.findOne({ _id: req.params.id });
     if (existTask) {
         existTask.task = task;
-        existTask.active = active;
+        existTask.active = active
         const updatedTask = await existTask.save();
         res.status(200).jason({
             success: true,
@@ -44,4 +44,76 @@ exports.updateTask = asyncHandler(async (req, res) => {
         })
     }
 
+})
+
+/**
+ * @desc For Delete Task
+ * @route /api/task/:id
+ * @access Public
+ */
+exports.deleteTask = asyncHandler(async (req, res) => {
+    const existTask = await Todo.findOne({ _id : req.params.id})
+    if(existTask){
+        await existTask.remove();
+        res.status(200).json({
+            success: true,
+            message: 'Task is deleted successfully'
+        })
+    }else{
+        res.status(401).json({
+            success: false,
+            data: null,
+            message: 'Task is Not Found'
+        })
+    }
+   
+})
+
+
+/**
+ * @desc For Get Single Task
+ * @route /api/task/:id
+ * @access Public
+ */
+ exports.getSingleTask = asyncHandler(async (req, res) => {
+    const existTask = await Todo.findOne({ _id : req.params.id})
+    if(existTask){
+        res.status(200).json({
+            success: true,
+            data:existTask,
+            message: 'Task is fetched successfully'
+        })
+    }else{
+        res.status(401).json({
+            success: false,
+            data: null,
+            message: 'Task is Not Found'
+        })
+    }
+   
+})
+
+
+
+/**
+ * @desc For Get all Tasks
+ * @route /api/task
+ * @access Public
+ */
+ exports.getAllTasks = asyncHandler(async (req, res) => {
+    const allTasks = await Todo.find({})
+    if(allTasks){
+        res.status(200).json({
+            success: true,
+            data:allTasks,
+            message: 'All Tasks are deleted successfully'
+        })
+    }else{
+        res.status(401).json({
+            success: false,
+            data: null,
+            message: 'Tasks are Not Found'
+        })
+    }
+   
 })
