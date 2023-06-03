85fe09bb26793265058e01fb7eb596b8cdc473b3
Piyush1411 <piyushggupta1411@gmail.com>
2023-06-03 16:58:35 +0530

    second commit

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const asyncHandeler = require('express-async-handler');
+const asyncHandler = require('express-async-handler');
 
 const Todo = require('../models/todoModel');
 
@@ -9,12 +9,39 @@ const Todo = require('../models/todoModel');
 * @access Public
 */
 
-exports.createTask = asyncHandeler(async (req, res) => {
-    const {task, active} = req.body
-    const todo = await Todo.create({task, active});
+exports.createTask = asyncHandler(async (req, res) => {
+    const { task, active } = req.body
+    const todo = await Todo.create({ task, active });
     res.status(201).json({
         success: true,
         data: todo,
         message: 'Task is created successfully'
     })
+})
+
+/*
+* @desc For update Task
+* @route /api/task 
+* @access Public 
+*/
+exports.updateTask = asyncHandler(async (req, res) => {
+    const { task, active } = req.body
+    const existTask = await Todo.findOne({ _id: req.params.id });
+    if (existTask) {
+        existTask.task = task;
+        existTask.active = active;
+        const updatedTask = await existTask.save();
+        res.status(200).jason({
+            success: true,
+            data: updatedTask,
+            message: 'Task is updated successfully'
+        })
+    } else {
+        res.status(401).jason({
+            success: false,
+            data: null,
+            message: 'Task is not found'
+        })
+    }
+
 })
--- routes/todo.js
+++ routes/todo.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {createTask} = require('../controllers/todoController');
+const {createTask, updateTask} = require('../controllers/todoController');
 
 router.route('/').post(createTask);
+router.route('/:id').put(updateTask);
 
 module.exports = router;
